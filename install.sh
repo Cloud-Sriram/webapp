988e936432e26f7817d59f922f8f7225b737c5a5
sriramvoruganti <sriramaries28@gmail.com>
2023-10-25 02:59:25 -0400

    testing-5

--- install.sh
+++ install.sh
@@ -8,92 +8,96 @@
 # sudo apt install nodejs
 # node -v
 # npm -v
-
+ 
 # #npm ci
-
+ 
 # echo "Sriram"
-
+ 
 # sudo apt-get install mariadb-server unzip -y
-
+ 
 # sudo systemctl start mysql
 # sudo systemctl enable mysql
 # sudo systemctl status mysql
 # sudo apt-get install expect
-
+ 
 # echo "Sriram"
-
+ 
 # sudo pwd
-
+ 
 # echo "Sriram"
-
+ 
 # sudo ls -ltrh
-
+ 
 # expect <<EOF
 # set timeout -1
 # spawn sudo mysql_secure_installation
-
+ 
 # expect "Enter current password for root (enter for none):"
 # send "\r"
-
+ 
 # expect "Set root password? [Y/n]"
 # send "n\r"
-
+ 
 # expect "Remove anonymous users? [Y/n]"
 # send "n\r"
-
+ 
 # expect "Disallow root login remotely? [Y/n]"
 # send "n\r"
-
+ 
 # expect "Remove test database and access to it? [Y/n]"
 # send "n\r"
-
+ 
 # expect "Reload privilege tables now? [Y/n]"
 # send "Y\r"
-
+ 
 # expect eof
 # EOF
-
+ 
 # # sudo mysql -u root -e "create user mohan identified by 'password'"
 # # sudo mysql -u root -e "create database test"
 # # sudo mysql -u root -e "grant all previliges on test.* to 'sriram'@'localhost' identified by 'password'"
-
+ 
 # sudo mysql -u root -e "create user 'sriram'@'localhost' identified by 'password'"
 # sudo mysql -u root -e "create database testing"
 # sudo mysql -u root -e "grant all privileges on testing.* to 'sriram'@'localhost' identified by 'password'"
 # #npm install --save
 # #npm fund
-
+ 
 # echo "list of databases"
-
+ 
 # sudo pwd
-
+ 
 # echo "below are current repo folders"
-
+ 
 # sudo ls -ltrh
-
-
-
+ 
+ 
+ 
 # cd ~/ && unzip webapp.zip
-
+ 
 # npm ci
 # npm install --save
 # npm fund
-
+ 
 sleep 30
 sudo apt-get update
 sudo apt-get install -y unzip curl
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install -y nodejs
 node -v
-
+ 
 sudo useradd -m -p $(openssl passwd -1 password) webapp
-
+ 
 # sudo mysql -u root -e "SHOW DATABASES";
-
-unzip /home/admin/webapp.zip -d /home/admin/webapp
-
+ 
+sudo mkdir /home/webapp/webapp
+ 
+sudo cp /home/admin/webapp.zip /home/webapp/webapp/
+ 
+sudo unzip -d /home/webapp/webapp/webapp.zip
+ 
 echo "check webapp.zip"
-cd /home/admin/webapp || exit
+sudo ls -ltrah /home/webapp/
 # rm -r .env
 # cat <<EOL > .env
 # DB_HOST=127.0.0.1
@@ -107,42 +111,47 @@ cd /home/admin/webapp || exit
 # DB_POOL_IDLE=10000
 # EOL
 # echo "The .env file has been created."
-
+ 
+cd /home/webapp/
+ 
 npm ci
-
+ 
 npm install --save
-
+ 
 npm fund
-
+ 
 # echo "DATABASE_HOST: mohan.c4tltzid5dl3.us-east-1.rds.amazonaws.com" >> ~/webapp/.env
-
+ 
 # echo "DATABASE_USER: mohan" >> ~/webapp/.env
-
+ 
 # echo "DATABASE_NAME: test" >> ~/webapp/.env
-
+ 
 # echo "DATABASE_PASSWORD: password" >> ~/webapp/.env
-
+ 
 # echo "PORT: 8087" >> ~/webapp/.env
-
+ 
 ls -ltrah
-
-cat ~/webapp/.env 
-
-cd ~/
-
+ 
+#cat ~/webapp/.env
+ 
+cd /home/webapp/
+ 
 sudo chown -R webapp:webapp webapp
-
+ 
+cd /home/admin
+ 
 sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
-
+ 
 sudo systemctl daemon-reload
-
+ 
 sudo systemctl enable webapp
-
+ 
 sudo systemctl start webapp
-
+ 
 sudo echo $?
-
+ 
 sleep 20
 sudo systemctl status webapp
-
+ 
 sudo echo $?
+has context menu
