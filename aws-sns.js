92fd3cabfa7c9accacf1e3ec6575a78a7c4c49c5
sriramvoruganti <sriramaries28@gmail.com>
2023-11-28 22:39:11 -0500

    demo-32

--- aws-sns.js
+++ aws-sns.js
@@ -1,87 +1,65 @@
-require('dotenv').config();
- 
-const logger = require('./logger/developmentLogs.js');
- 
+const logger = require('./logger/log');
 const AWS = require('aws-sdk');
- 
-const awsProfile = 'sriram-dev'
- 
-AWS.config.credentials = new AWS.SharedIniFileCredentials({ profile: awsProfile });
- 
-const awsRegion = process.env.AWSREGION;
- 
-const roleArn = 'arn:aws:iam::175506346862:role/snsfullaccessforlocal';
- 
-AWS.config.update({ region: awsRegion });
- 
-const sts = new AWS.STS();
- 
-const sns = new AWS.SNS();
- 
-const topicArn = process.env.SNSTOPICARN;
- 
-const publishSnsMessage = async(req,res) => {
-  try
-  {
+require('dotenv').config();
+//const awsProfile = 'sbanala2dev';
+// AWS.config.credentials = new AWS.SharedIniFileCredentials({ profile: awsProfile });
+const snsTopicArn = process.env.ARNSNSTOPIC;
+const snsregion = process.env.AWS_SNS_REGION;
 
- 
-    const message = `Assignment posted by User - ${req.user_email} for Assignment ID - ${req.assignmentId}`;
-    const attributes = {
-      submission_url : {
-        DataType: "String",
-        StringValue: req.submission_url
-      },
-      user_email : {
-        DataType: "String",
-        StringValue: req.user_email
-      },
-      assignmentID : {
-        DataType: "String",
-        StringValue: req.assignmentID,
-      },
-      submissionID : {
-        DataType: "String",
-        StringValue: req.submissionID,
-      },
-      assignmentName : {
-        DataType: "String",
-        StringValue: req.assignmentName,
-      }
+AWS.config.update({ region: snsregion });
+const sns = new AWS.SNS();
+
+const publishMessageToSNS = async (request, response) => {
+  const message = `Assignment posted by User - ${request.user_email} for Assignment ID - ${request.assignmentID}`;
+
+  const attributes = {
+    submissionUrl: {
+      DataType: "String",
+      StringValue: request.submissionUrl
+    },
+    user_email: {
+      DataType: "String",
+      StringValue: request.user_email
+    },
+    assignmentId: {
+      DataType: "String",
+      StringValue: request.assignmentId,
+    },
+    assignment_Name: {
+      DataType: "String",
+      StringValue: request.assignment_Name,
+    },
+    submissionID: {
+      DataType: "String",
+      StringValue: request.submissionID,
     }
- 
- 
- 
-    console.log(attributes);
- 
-    const params = {
-      TopicArn: topicArn,
-      Message: message,
-      MessageAttributes : attributes
-    };
- 
-    console.log(params);
- 
-    sns.publish(params, (err, data) => {
-      if (err)
-      {
-        logger.info(`Error in publishing the message to SNS`)
-        console.log(err);
-        return
-      }
-      else
-      {
-        logger.info( ` Successfully Published Message to SNS with Message ID -  ${data.MessageId}`);
-        return
-      }
+  };
+
+  const params = {
+    TopicArn: snsTopicArn,
+    Message: message,
+    MessageAttributes: attributes
+  };
+
+  console.log(params);
+
+  try {
+    const data = await new Promise((resolve, reject) => {
+      sns.publish(params, (error, result) => {
+        if (error) {
+          logger.error(`Error in publishing the message to SNS: ${error.message}`);
+          reject(error);
+        } else {
+          logger.info(`Successfully Published Message to SNS with Message ID - ${result.MessageId}`);
+          resolve(result);
+        }
+      });
     });
+
+    return data;
+  } catch (error) {
+    return error;
   }
-  catch(error)
-  {
-    logger.error(`Error in publishing the message to SNS: ${error.message}`);
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
- 
-}
- 
-module.exports = publishSnsMessage;
+};
+
+module.exports = publishMessageToSNS;
