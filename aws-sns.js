15316f914d9e355a4647acba8b3f616a739b82c1
Sriram-Northeastern <113790442+Sriram-Northeastern@users.noreply.github.com>
2023-11-29 06:27:59 -0500

    Demo 35 (#65)
    * demo-35
    
    * demo-35 changes
    
    ---------
    
    Co-authored-by: sriramvoruganti <sriramaries28@gmail.com>

--- aws-sns.js
+++ aws-sns.js
@@ -1,17 +1,21 @@
-const logger = require('./logger/log');
+// const logger = require('./logger/log');
 const AWS = require('aws-sdk');
 require('dotenv').config();
 //const awsProfile = 'demo-account';
-// AWS.config.credentials = new AWS.SharedIniFileCredentials({ profile: awsProfile });
+//AWS.config.credentials = new AWS.SharedIniFileCredentials({ profile: awsProfile });
+
 const snsTopicArn = process.env.ARNSNSTOPIC;
 const snsregion = process.env.AWS_SNS_REGION;
 
-AWS.config.update({ region: snsregion });
+
+
+AWS.config.update({ region: snsregion,  });
 const sns = new AWS.SNS();
 
 const publishMessageToSNS = async (request, response) => {
   const message = `Assignment posted by User - ${request.user_email} for Assignment ID - ${request.assignmentID}`;
 
+  console.log(request, "here is the coins");
   const attributes = {
     submissionUrl: {
       DataType: "String",
@@ -19,7 +23,7 @@ const publishMessageToSNS = async (request, response) => {
     },
     user_email: {
       DataType: "String",
-      StringValue: request.user_email
+      StringValue: request.userEmail
     },
     assignmentId: {
       DataType: "String",
@@ -27,11 +31,11 @@ const publishMessageToSNS = async (request, response) => {
     },
     assignment_Name: {
       DataType: "String",
-      StringValue: request.assignment_Name,
+      StringValue: request.assignmentTitle,
     },
     submissionID: {
       DataType: "String",
-      StringValue: request.submissionID,
+      StringValue: request.submissionId,
     }
   };
 
@@ -47,17 +51,20 @@ const publishMessageToSNS = async (request, response) => {
     const data = await new Promise((resolve, reject) => {
       sns.publish(params, (error, result) => {
         if (error) {
-          logger.error(`Error in publishing the message to SNS: ${error.message}`);
+          console.log("error publis", error);
+          // logger.error(`Error in publishing the message to SNS: ${error.message}`);
           reject(error);
         } else {
-          logger.info(`Successfully Published Message to SNS with Message ID - ${result.MessageId}`);
+          console.log("success publis");
+          // logger.info(`Successfully Published Message to SNS with Message ID - ${result.MessageId}`);
           resolve(result);
         }
       });
     });
-
+    console.log(data);
     return data;
   } catch (error) {
+    console.log(error);
     return error;
   }
 };
